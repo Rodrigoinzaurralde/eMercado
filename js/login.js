866a54bfe54068a11b528901ec2eea85ccb41ef6
Rodrigoinzaurralde <rodrigoinzaurralde97@gmail.com>
2025-08-21 19:22:19 -0300

    Se cambia el orden de las validaciones

--- js/login.js
+++ js/login.js
@@ -6,18 +6,18 @@ document.querySelector('.login__button').addEventListener('click', function(even
     const user = document.querySelector('.user__name').value.trim();
     const password = document.querySelector('.user__password').value.trim();
     const errorMsg = document.querySelector('.error__msg');
-    if(user && password && password.length >=8 ){
-        localStorage.setItem('user', user);
-        window.location.href = 'index.html';
-}   else if(!user || !password){
+    if(!user || !password){
         errorMsg.classList.remove('error__pass');
         errorMsg.textContent = 'Rellena todos los datos';
     }else if(!validarEmail(user)){
         errorMsg.classList.add('error__pass');
         errorMsg.textContent = 'El usuario no tiene un formato válido';
-    }else{
+    }else if(password.length < 8){
         errorMsg.classList.add('error__pass');
         errorMsg.textContent = 'La contraseña debe tener al menos 8 caracteres';
+    }else{
+        localStorage.setItem('user', user);
+        window.location.href = 'index.html';
     }
 });
 
