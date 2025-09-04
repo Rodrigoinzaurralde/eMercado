7b4e08c33348c1eda2897ddc70d359738089e36d
Lukichi-ve <lucasve45@gmail.com>
2025-09-04 16:56:16 -0300

    Filtro

--- js/init.js
+++ js/init.js
@@ -23,7 +23,7 @@ if(menu.style.display === "none"){
 }
 })
 document.getElementById("closeSession").addEventListener("click", ()=>{
-  localStorage.clear();
+  localStorage.removeItem("user");
 });
 //-----------------------------------------------------------------------------
 
